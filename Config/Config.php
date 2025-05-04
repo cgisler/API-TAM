a51c56b7104ee8a74e7593d336639dfba37d63c6
Wolfen Gisler <gisler2001@hotmail.com>
2025-05-04 15:49:47 -0600

    Actualización de archivo config

--- Config/Config.php
+++ Config/Config.php
@@ -5,19 +5,19 @@
 	const BASE_URL = "https://api.tamtransmisiones.com.mx/";
 
     //Datos de conexión a Base de Datos
-	/*const CONNECTION = true;
-	const DB_HOST = "localhost";
-	const DB_NAME = "admin_";
-	const DB_USER = "tam_root";
-	const DB_PASSWORD = "Q00t?b0b7";
-	const DB_CHARSET = "utf8"; */
-
 	const CONNECTION = true;
+	const DB_HOST = "localhost";
+	const DB_NAME = "tamshop";
+	const DB_USER = "tamshop";
+	const DB_PASSWORD = "Q?!mi1t1Ax3iWuww";
+	const DB_CHARSET = "utf8"; 
+
+	/*const CONNECTION = true;
 	const DB_HOST = "localhost";
 	const DB_NAME = "admin_";
 	const DB_USER = "root";
 	const DB_PASSWORD = "";
-	const DB_CHARSET = "utf8";
+	const DB_CHARSET = "utf8";*/
 
 
 
